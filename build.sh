1e4efeafba5e7d352a5fdfdec33c8b7eca18ffdc
Hideaki Muranami <muranamihideaki@gmail.com>
2019-04-17 23:52:04 +0900

    modified build.sh

--- build.sh
+++ build.sh
@@ -12,41 +12,41 @@ if [ $(curl \
 then
   echo "No changes to commit at Weblate"
   exit 0
-fi
-
-if [ $(curl \
- -s \
- -d operation=commit \
- -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
- http://www3420ue.sakura.ne.jp:8080/api/components/tcw/tcw20/repository/ \
-| jq -r '.result') = false ]
-then
-  echo "Commit at Weblate Failure."
-  exit 1
 else
-  echo "Commit at Weblate Success."
+  if [ $(curl \
+   -s \
+   -d operation=commit \
+   -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+   http://www3420ue.sakura.ne.jp:8080/api/components/tcw/tcw20/repository/ \
+  | jq -r '.result') = false ]
+  then
+    echo "Commit at Weblate Failure."
+    exit 1
+  else
+    echo "Commit at Weblate Success."
+  fi
+  
+  echo
+  
+  while :
+  do
+   if [ $(curl \
+    -s \
+    -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+    http://www3420ue.sakura.ne.jp:8080/api/projects/tcw/repository/ \
+   | jq -r '.needs_push') = true ]
+   then
+     echo "Waiting for Weblate to push changes..."
+     sleep 3
+   else
+    break
+   fi
+  done
+  
+  echo "git pull origin localize_ja"
+  git pull origin localize_ja
+  echo
 fi
-
-echo
-
-while :
-do
- if [ $(curl \
-  -s \
-  -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
-  http://www3420ue.sakura.ne.jp:8080/api/projects/tcw/repository/ \
- | jq -r '.needs_push') = true ]
- then
-   echo "Waiting for Weblate to push changes..."
-   sleep 3
- else
-  break
- fi
-done
-
-echo "git pull origin localize_ja"
-git pull origin localize_ja
-echo
 
 for FILE in tcw20 tcw22 tcw24 tcw27 tcw29 testing_and_building
 do
@@ -73,10 +73,39 @@ do
   fi
 done
 
-echo
-git add .
-git commit -m "update translations"
-echo
-echo "git push origin localize_ja"
-git push origin localize_ja
+#if [ $(git diff origin/localize_ja --name-only | wc -l) -ne 0 ]
+#then
+#  git status
+#fi
 
+if [ $(git status | grep "Changes not staged for commit" | wc -l) -ne 0 ]
+then
+  git status
+  echo
+  git add .
+  git commit -m "update translations"
+  echo
+fi
+
+if [ $(git status | grep "Your branch is ahead of" | wc -l) -ne 0 ]
+then
+  echo
+  echo "git push origin localize_ja"
+  git push origin localize_ja
+  echo
+  
+  # pull at Weblate
+  if [ $(curl \
+   -s \
+   -d operation=pull \
+   -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+   http://www3420ue.sakura.ne.jp:8080/api/components/tcw/tcw20/repository/ \
+  | jq -r '.result') = false ]
+  then
+    echo "Pull at Weblate Failure."
+    exit 1
+  else
+    echo "Pull at Weblate Success."
+    echo
+  fi
+fi
